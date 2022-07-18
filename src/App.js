3680cf522f3b6c721fd77d2b2dbd4e1bbe3bbe7f
Lee Dohoon <mero.japan@gmail.com>
2022-07-18 21:30:46 +0900

    2022/07/18 react-todo-app 연습

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component {
+
+  state = {
+    todoData : [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false,
+      },
+    ],
+    value: "",
+  }
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    }
+  }
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter(data => data.id !== id);
+    this.setState({ todoData: newTodoData });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>To do list</h1>
+          </div>
+
+          {this.state.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+            </div>
+          ))}
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
